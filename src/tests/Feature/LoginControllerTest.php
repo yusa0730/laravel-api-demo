6fc31bd5236765aaf05eba4c52688aec2efe150f
yusa0730 <isizawa5226@gmail.com>
2024-12-03 02:04:04 +0900

    login test

--- src/tests/Feature/LoginControllerTest.php
+++ src/tests/Feature/LoginControllerTest.php
@@ -11,10 +11,31 @@ class LoginControllerTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    public function test_login_response_200()
+	{
+		$requestBody = [
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'password' => 'password',
+            'confirmPassword' => 'password',
+		];
+		$response = $this->postJson('/api/v1/auth/register', $requestBody);
 
-        $response->assertStatus(200);
-    }
+        $loginRequestBody = [
+            'email' => 'test@gmail.com',
+            'password' => 'password'
+		];
+		$loginResponse = $this->postJson('/api/v1/auth/login', $loginRequestBody);
+
+		$loginResponse->assertStatus(200);
+
+		$loginResponse->assertJson([
+			'status' => 200,
+			'data' => [
+				'userId' => 1,
+				'accessToken' => true,
+			],
+			'message' => 'User Login Successfully'
+		]);
+	}
 }
